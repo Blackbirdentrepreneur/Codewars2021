ecbeaf99aaf884336e721a77d7645a50ebaf35eb
Blackbirdentrepreneur <66925133+Blackbirdentrepreneur@users.noreply.github.com>
2021-03-06 10:31:45 -0800

    Update prob06.py

--- Codewars2021.py/prob06.py
+++ Codewars2021.py/prob06.py
@@ -6,6 +6,8 @@ for i in words:
     for letter in i:
         e = ord(str(letter))
         d = chr(e - 5)
+        if d == '=': 
+            d = chr(e + 21)
         result.append(d)
 for i in words:
     print(i)
